fbd689eac3b645dcd31116a5c5bcd4e1fab6c16c
Alexander Logger <intagger@live.ru>
2014-11-21 17:57:07 +0300

    Added TelegramClient + Schema layer 18.

---
+++ src/SharpTelegram.Tests/TelegramClientTests.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TelegramClientTests.cs">
+//   Copyright (c) 2013-2014 Alexander Logger. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Moq;
+using NUnit.Framework;
+using SharpMTProto.Transport;
+
+namespace SharpTelegram.Tests
+{
+    [TestFixture]
+    public class TelegramClientTests
+    {
+        public void Should_init_connection()
+        {
+            // TODO;
+        }
+    }
+}
