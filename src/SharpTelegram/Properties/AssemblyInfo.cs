01f74885a79269b16f00ae863eb1aab4e8e2c302
Alexander Logger <intagger@live.ru>
2014-11-22 13:21:17 +0300

    Changed UpdateAssemblyInfo strategy.
    Removed GitVersionTask from the project and added a task to the build
    script:
    
    task UpdateAssemblyInfo {
        exec { gitversion.exe /updateassemblyinfo true }
    }

--- src/SharpTelegram/Properties/AssemblyInfo.cs
+++ src/SharpTelegram/Properties/AssemblyInfo.cs
@@ -8,3 +8,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("SharpTelegram")]
 [assembly: AssemblyDescription("Client library for Telegram API.")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
